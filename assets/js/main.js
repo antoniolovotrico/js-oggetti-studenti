7a3580f941fad4f6a1054539eb7e7f78fce14960
Antonio <antonio.lovotrico@icloud.com>
2020-12-03 15:22:03 +0100

    created array with student object and made for loop to stamp name ad surname and age

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,41 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
 var student = {
-    "nome" : "",
-    "cognome" : "",
-    "eta" : ""
+    "nome" : "Thomas",
+    "cognome" : "Shelby",
+    "eta" : "35"
 }
 
 for (var key in student) {
-    console.log(key);
+    console.log(key ,student[key]);
 }
+
 // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var classe = [
+    {
+        "nome" : "Thomas",
+        "cognome" : "Shelby",
+        "eta" : "35" 
+    },
+    {
+        "nome" : "Alfie",
+        "cognome" : "Solomons",
+        "eta" : "38" 
+    },
+    {
+        "nome" : "Elizabeth",
+        "cognome" : "Gray",
+        "eta" : "45" 
+    },
+    {
+        "nome" : "Arthur",
+        "cognome" : "Shelby",
+        "eta" : "39" 
+    }
+
+];
+
+for (var i = 0; i < classe.length; i++) {
+    
+    console.log(classe[i]);
+}
 // Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
